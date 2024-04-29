a947e43fb0d104309a52670ec2d7895acdafa219
Mohan <smohan0305@gmail.com>
2024-04-29 14:59:57 +0530

    first commit

---
+++ Java Problems/src/com/java/problems/Problem_64.java
@@ -0,0 +1,44 @@
+package com.java.problems;
+
+public class Problem_64 {
+
+	public static void main(String[] args) {
+		/*
+		 * Given an array of integers nums, return the number of good pairs.
+		 * 
+		 * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+		 * 
+		 * Example 1:
+		 * 
+		 * Input: nums = [1,2,3,1,1,3] Output: 4 Explanation: There are 4 good pairs
+		 * (0,3), (0,4), (3,4), (2,5) 0-indexed. Example 2:
+		 * 
+		 * Input: nums = [1,1,1,1] Output: 6 Explanation: Each pair in the array are
+		 * good. Example 3:
+		 * 
+		 * Input: nums = [1,2,3] Output: 0
+		 * 
+		 * Constraints:
+		 * 
+		 * 1 <= nums.length <= 100 1 <= nums[i] <= 100
+		 */
+		int[] nums = {1,2,3,1,1,3};
+		System.out.println(numIdenticalPairs(nums));
+	}
+	
+	private static int numIdenticalPairs(int[] nums) {
+		// get the array length
+		int len = nums.length;
+		int count = 0;
+		// based on length iterate the loop
+		for (int i = 0; i < len; i++) {
+			for ( int j = i; j < len; j++) {				
+				// current element and next element is equal to or less than
+				if (nums[i] == nums[j] && i < j) {
+					count++; // increment the count by 1
+				}
+			}
+		}
+		return count;
+	}
+}
