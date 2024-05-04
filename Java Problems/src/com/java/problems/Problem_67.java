e4c0c8d7bae577846b2fea91414345d32376f285
Mohan <smohan0305@gmail.com>
2024-05-04 20:03:28 +0530

    first commit

---
+++ Java Problems/src/com/java/problems/Problem_67.java
@@ -0,0 +1,56 @@
+package com.java.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_67 {
+
+	public static void main(String[] args) {
+		/*
+		 * Check if the sentence is pangram
+		 * 
+		 * A pangram is a sentence where every letter of the English alphabet appears at
+		 * least once.
+		 * 
+		 * Given a string sentence containing only lowercase English letters, return
+		 * true if sentence is a pangram, or false otherwise.
+		 * 
+		 * Example 1:
+		 * 
+		 * Input: sentence = "thequickbrownfoxjumpsoverthelazydog" Output: true
+		 * Explanation: sentence contains at least one of every letter of the English
+		 * alphabet. Example 2:
+		 * 
+		 * Input: sentence = "leetcode" Output: false
+		 * 
+		 * Constraints:
+		 * 
+		 * 1 <= sentence.length <= 1000 sentence consists of lowercase English letters.
+		 */
+		String sentence = "Pack my box with five dozen liquor jugs.";
+		System.out.println(checkIfPangram(sentence));
+	}
+
+	private static boolean checkIfPangram(String sentence) {
+		// remove white space and special character in the String
+		// manually convert all the character into lowerCase
+		sentence = sentence.replaceAll(" ", "").toLowerCase().replaceAll("[^a-z]", "");
+		System.out.println(sentence);
+		// get the String length
+		int len = sentence.length();
+		// create one new character arrayList
+		List<Character> pangram = new ArrayList<>();
+		// based on length iterate the loop
+		for (int i = 0; i < len; i++) {
+			// check pangram(character arrayList) not contains existing character than add
+			// that character
+			if (!pangram.contains(sentence.charAt(i))) {
+				// take each character from the string add it to pangram(character arrayList)
+				pangram.add(sentence.charAt(i));
+			}
+		}
+		// check pangram(character arrayList) equal to 26 character all the alphabet is
+		// present
+		return pangram.size() == 26; // return true otherwise return false
+	}
+}
