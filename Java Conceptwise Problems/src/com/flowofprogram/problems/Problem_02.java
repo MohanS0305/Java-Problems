fd5fe583aeb8fbe55addc4de033154e685d2ddb2
Mohan <smohan0305@gmail.com>
2024-05-08 20:42:21 +0530

    initial commit

---
+++ Java Conceptwise Problems/src/com/flowofprogram/problems/Problem_02.java
@@ -0,0 +1,15 @@
+package com.flowofprogram.problems;
+
+public class Problem_02 {
+
+	public static void main(String[] args) {
+		// sum two numbers
+		// take num1 and num2 to store two values
+		int num1 = 10, num2 = 15;
+		// add the num1 and num2 store it in sum
+		int sum = num1 + num2;
+		// after sum print the sum value
+		System.out.println(sum);
+	}
+
+}
