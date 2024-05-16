9923c3d7f4e7116038f43e1803f641e7ec7a7be2
Mohan <smohan0305@gmail.com>
2024-05-16 09:13:09 +0530

    initial commit

---
+++ Java Conceptwise Problems/src/com/firstjava/problems/Problem_03.java
@@ -0,0 +1,30 @@
+package com.firstjava.problems;
+
+import java.util.Scanner;
+
+public class Problem_03 {
+
+	public static void main(String[] args) {
+		// Write a program to input principal, time, and rate (P, T, R) from the user
+		// and find Simple Interest.
+
+		// ask input from user
+		Scanner input = new Scanner(System.in);
+		// take the input from user
+		System.out.println("Enter the simple interest principle here :");
+		// P is the initial amount of money.
+		int p = input.nextInt();
+		System.out.println("Enter the simple interest rate here :");
+		// R is the rate of interest per year, usually given as a percentage.
+		float r = input.nextFloat();
+		System.out.println("Enter the simple interest time here :");
+		// T is the time duration for which the money is borrowed or invested, usually given in years.
+		int t = input.nextInt();
+
+		// formula for simpleInterest is SI = P x R x T / 100
+		int si = (int) (p * r * t) / 100;
+		System.out.println(si);
+		input.close();
+	}
+
+}
