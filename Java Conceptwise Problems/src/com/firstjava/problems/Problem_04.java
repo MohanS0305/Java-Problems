6928767ec7ed2faeabe64a423ece32f9154460f1
Mohan <smohan0305@gmail.com>
2024-05-17 09:15:00 +0530

    initial commit

---
+++ Java Conceptwise Problems/src/com/firstjava/problems/Problem_04.java
@@ -0,0 +1,35 @@
+package com.firstjava.problems;
+
+import java.util.Scanner;
+
+public class Problem_04 {
+
+	public static void main(String[] args) {
+		// Take in two numbers and an operator (+, -, *, /) and calculate the value.
+		// (Use if conditions)
+		
+		// ask the input from user
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the 1st no :");
+		// take input from user
+		int n1 = input.nextInt();
+		System.out.println("Enter the 2nd no :");
+		int n2 = input.nextInt();
+		System.out.println("Enter any operator from one of these +, -, *, /");
+		char userCh = input.next().trim().charAt(0);
+		// based on user operator perform calculation
+		if (userCh == '+') {
+			System.out.println(String.format("Addition of %d & %d is %d", n1,n2, n1+n2));
+		} else if (userCh == '-') {
+			System.out.println(String.format("Subtraction of %d & %d is %d", n1,n2, n1-n2));
+		} else if (userCh == '*') {
+			System.out.println(String.format("Multiplication of %d & %d is %d", n1,n2, n1*n2));
+		} else if (userCh == '/') {
+			System.out.println(String.format("Division of %d & %d is %d", n1,n2, n1/n2));
+		} else {
+			System.out.println("Pls enter correct choice.");
+		}
+		input.close();
+	}
+
+}
