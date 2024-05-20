03690799e0962ec01c8192a8ed4fee952fff1dc5
Mohan <smohan0305@gmail.com>
2024-05-20 18:10:31 +0530

    initial commit

---
+++ Java Conceptwise Problems/src/com/firstjava/problems/Problem_07.java
@@ -0,0 +1,22 @@
+package com.firstjava.problems;
+import java.util.*;
+public class Problem_07 {
+
+	public static void main(String[ ] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the value for n");
+		int n=input.nextInt();
+		int[ ] a=new int[n];
+		input.close();
+		int i;
+		a[0]=0;
+		a[1]=1;
+		for(i=2;i<n;i++) {
+			a[i]=a[i-1]+a[i-2];
+		}
+		System.out.println("Numbers are:");
+		for(i=0;i<n;i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+}
