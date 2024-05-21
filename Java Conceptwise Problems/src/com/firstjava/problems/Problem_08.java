7977c3f50acff22f8fefe8d22af532398379484d
Mohan <smohan0305@gmail.com>
2024-05-21 08:02:29 +0530

    initial commit

---
+++ Java Conceptwise Problems/src/com/firstjava/problems/Problem_08.java
@@ -0,0 +1,40 @@
+package com.firstjava.problems;
+
+import java.util.Scanner;
+
+public class Problem_08 {
+
+	public static void main(String[] args) {
+		// To find out whether the given String is Palindrome or not.
+
+		/*
+		 * palindrome means given string and reverse string both are same it's a
+		 * palindrome otherwise not. for example mom dad
+		 */
+		
+		// ask the user input
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter some name :");
+		// take the input from user
+		String userInput = input.nextLine();
+		//get the length of the string
+		int len = userInput.length();
+		// for storing reverse String
+		StringBuilder str = new StringBuilder();
+		// based on length iterate the loop
+		for (int i = len - 1; i >= 0; i--) {
+			// take each character from original string
+			// and add it in str
+			str.append(userInput.charAt(i));
+		}
+		// check user string and str string both are matched 
+		// if it is matched 
+		if (userInput.equals(str.toString())) {
+			System.out.println("Palindrome"); // print palindrome
+		} else { // otherwise
+			System.out.println("Not palindrome"); // print not palindrome
+		}
+		input.close();
+	}
+
+}
